eb2b62bba712a8ba9f424c90df8507c9242eca9e
Kuldeep Nayak <138770146+Kuldeep-Nayak@users.noreply.github.com>
2024-11-23 00:42:30 +0530

    Delete src directory

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the app header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Stack Overflow Clone/i);
-  expect(headerElement).toBeInTheDocument();
-});
